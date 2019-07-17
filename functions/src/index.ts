24167d1714ab4ce0fa32c7cc0aaec0190f885229
salah3x <salah.loukili@gmail.com>
2019-07-17 23:03:22 +0100

    Add users to firestore after signing up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,1 +1,18 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+export const AddUser = functions.auth
+  .user()
+  .onCreate((user: functions.auth.UserRecord, _: functions.EventContext) => {
+    const newUser = {
+      id: user.uid,
+      name: user.displayName,
+      photo: user.photoURL
+    };
+    return admin
+      .firestore()
+      .collection('users')
+      .add(newUser);
+  });
